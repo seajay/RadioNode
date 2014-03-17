6ed0c21f8a8bb16ef23b750ab57bf0c33e788038
Chris Jones <github@cjones.org.uk>
2014-03-17 21:39:49 +0000

    NOW it compiles

--- RadioNode.ino
+++ RadioNode.ino
@@ -16,14 +16,14 @@
 RF24 radio(9,10);
 
 // Radio pipe addresses. Node N recieves on pipe[N].
-const uint64_t pipes[2] = { 0xCCCCCCCC00, 0xCCCCCCCC01 };
+const uint64_t pipes[2] = { 0xCCCCCCCC00LL, 0xCCCCCCCC01LL };
 
 // Start up as node 0 (master) ### Todo read this from EEPROM
 uint8_t node_id = 0;
 
 // ### Todo calc my max payload size. 32 is the radio maximum
-#define RX_BUF_SIZE 32
-char rx_buf[RX_BUF_SIZE];
+#define BUF_SIZE 32
+char rx_buf[BUF_SIZE];
 
 enum msg_types {
   temp,
@@ -31,18 +31,21 @@ enum msg_types {
   button,
 };
 
-typedef struct msg {
+#define DATA_LEN 20 // ## probably 20ish, maybe check this
+
+typedef struct msg_t {
   byte src_node;
   byte dest_node;
   enum msg_types msg_type;
-  union data
+  union
   {
     float temp;
+    char txt[DATA_LEN];  
     //  ## todo other message types
-  }
-} msg;
+  };
+} msg_t;
 
-msg *rx_msg;
+msg_t * msg;
 
 void setup_pipes(uint8_t node)
 {
@@ -72,7 +75,7 @@ void setup(void)
   // Use the max delay and number of retries, we care more about reliability than performance
   radio.setRetries(15,15);
 
-  radio.setPayloadSize(RX_BUF_SIZE);
+  radio.setPayloadSize(BUF_SIZE);
 
   setup_pipes(node_id);
 
@@ -88,10 +91,10 @@ void loop(void)
   {
     radio.read( rx_buf, sizeof(rx_buf) );
     Serial.println("Recieved message");
-    rx_buf[RX_BUF_SIZE-1] =0; // Make sure null terminated ### don't leave this in!
-    Serial.println(rx_buf);
+    rx_buf[BUF_SIZE-1] =0; // Make sure null terminated ### don't leave this in!
+    Serial.println((char *)rx_buf);
 
-    msg = &rx_buf;
+    msg = (msg_t *)&rx_buf;
     Serial.print("src ");
     Serial.println(msg->src_node);
     Serial.print("dest ");
@@ -104,134 +107,40 @@ void loop(void)
     else {
       Serial.println(" someone else");
     }
-    // Delay just a little bit to let the other unit
-    // make the transition to receiver ### is this necessary?
-    delay(20);
   }
 
   if ( Serial.available() > 2)
   {
     switch(Serial.read()) {
       case 'n':
-        node_id = Serial.read(); // ### Todo range check
+        node_id = (char)Serial.read() - '0'; // ### Todo range check
         setup_pipes(node_id);
         Serial.print("Set node id to ");
         Serial.println(node_id);
         break;
+  
       case 's':  
-        // Send string      
+        // Send message      
+        
+        radio.stopListening(); // First, stop listening so we can talk.
+        msg->src_node = node_id;
+        msg->dest_node = (char)Serial.read() - '0';
+        int n;
+        for (n = 0; n < DATA_LEN-1; ++n){
+          msg->txt[n] = Serial.read();  // Serial.read returns -1 if there is nothing to read so this will fill with 0xFF 
+        }
+        msg->txt[n] = 0; // terminator
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //
-  // Ping out role.  Repeatedly send the current time
-  //
-
-  if (role == role_ping_out)
-  {
-    // First, stop listening so we can talk.
-    radio.stopListening();
-
-    // Take the time, and send it.  This will block until complete
-    unsigned long time = millis();
-    printf("Now sending %lu...",time);
-    bool ok = radio.write( &time, sizeof(unsigned long) );
+        bool ok = radio.write( msg, BUF_SIZE );
+        if (ok)
+          printf("ok...");
+        else
+          printf("failed.\n\r");
     
-    if (ok)
-      printf("ok...");
-    else
-      printf("failed.\n\r");
-
-    // Now, continue listening
-    radio.startListening();
-
-    // Wait here until we get a response, or timeout (250ms)
-    unsigned long started_waiting_at = millis();
-    bool timeout = false;
-    while ( ! radio.available() && ! timeout )
-      if (millis() - started_waiting_at > 200 )
-        timeout = true;
-
-    // Describe the results
-    if ( timeout )
-    {
-      printf("Failed, response timed out.\n\r");
-    }
-    else
-    {
-      // Grab the response, compare, and send to debugging spew
-      unsigned long got_time;
-      radio.read( &got_time, sizeof(unsigned long) );
-
-      // Spew it
-      printf("Got response %lu, round-trip delay: %lu\n\r",got_time,millis()-got_time);
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-  //
-  // Pong back role.  Receive each packet, dump it out, and send it back
-  //
-
-  if ( role == role_pong_back )
-  {
-    // if there is data ready
-    if ( radio.available() )
-    {
-      // Dump the payloads until we've gotten everything
-      unsigned long got_time;
-      bool done = false;
-      while (!done)
-      {
-        // Fetch the payload, and see if this was the last one.
-        done = radio.read( &got_time, sizeof(unsigned long) );
-
-        // Spew it
-        printf("Got payload %lu...",got_time);
-
-	// Delay just a little bit to let the other unit
-	// make the transition to receiver
-	delay(20);
-      }
-
-      // First, stop listening so we can talk
-      radio.stopListening();
-
-      // Send the final one back.
-      radio.write( &got_time, sizeof(unsigned long) );
-      printf("Sent response.\n\r");
-
-      // Now, resume listening so we catch the next packets.
-      radio.startListening();
+        // Now, continue listening
+        radio.startListening();
+        break;
     }
   }
-
-  //
-  // Change roles
-  //
-
 }
-// vim:cin:ai:sts=2 sw=2 ft=cpp
+
